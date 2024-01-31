e3ac244842b977d7239df0e0703b692139a1841c
Avery Pfeiffer <avery.pfeiffer@virtru.com>
2024-01-31 11:04:47 -0500

    Add llmgen WIP integration and test command

--- cmd/llmgen.go
+++ cmd/llmgen.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+
+	"github.com/opentdf/tructl/pkg/llmgen"
 	"github.com/spf13/cobra"
 )
 
@@ -8,7 +11,20 @@ var llmgenCmd = &cobra.Command{
 	Use:   "llmgen",
 	Short: "Just a simple test mode for our llmgen integration",
 	Run: func(cmd *cobra.Command, args []string) {
-		println("llmgen test - PASS")
+		var userQuery string
+		// var function string
+		if len(args) > 0 {
+			userQuery = args[0]
+		}
+
+		if userQuery == "" {
+			fmt.Println("Error: No query provided. Please provide a query.")
+			return
+		}
+		print("thinking...\n\n")
+		// llmgen.RawInference(function, userQuery)
+		// llmgen.RawInference(userQuery)
+		llmgen.KnowledgeBaseChat("What in the heck is the DSP?")
 	},
 }
 
