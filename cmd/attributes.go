525c4d82b534d24763c5bcd37def0cb9627e3432
Ryan Schumacher <jschumacher@virtru.com>
2024-02-05 16:25:40 -0600

    feat(policy): update attribute crud

--- cmd/attributes.go
+++ cmd/attributes.go
@@ -1,261 +1,210 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
-// import (
-// 	"fmt"
-// 	"os"
-// 	"strconv"
-// 	"strings"
+import (
+	"fmt"
+	"strings"
 
-// 	"github.com/opentdf/tructl/pkg/cli"
-// 	"github.com/spf13/cobra"
-// )
+	"github.com/opentdf/tructl/pkg/cli"
+	"github.com/spf13/cobra"
+)
 
-// var (
-// 	attrValues           []string
-// 	groupBy              []string
-// 	resourceDependencies []string
+var (
+	attrValues []string
 
-// 	attributeCommands = []string{
-// 		attributesCreateCmd.Use,
-// 		attributeGetCmd.Use,
-// 		attributesListCmd.Use,
-// 		attributeUpdateCmd.Use,
-// 		attributesDeleteCmd.Use,
-// 	}
+	attributeCommands = []string{
+		attributesCreateCmd.Use,
+		attributeGetCmd.Use,
+		attributesListCmd.Use,
+		attributeUpdateCmd.Use,
+		attributesDeleteCmd.Use,
+	}
 
-// 	attributesCmd = &cobra.Command{
-// 		Use:   "attributes",
-// 		Short: "Manage attributes [" + strings.Join(attributeCommands, ", ") + "]",
-// 		Long: `
-// Attributes - commands to manage attributes within the platform.
+	attributesCmd = &cobra.Command{
+		Use:   "attributes",
+		Short: "Manage attributes [" + strings.Join(attributeCommands, ", ") + "]",
+		Long: `
+Attributes - commands to manage attributes within the platform.
 
-// Attributes are used to to define the properties of a piece of data. These attributes will then be
-// used to define the access controls based on subject encodings and entity entitlements.
-// `,
-// 	}
+Attributes are used to to define the properties of a piece of data. These attributes will then be
+used to define the access controls based on subject encodings and entity entitlements.
+`,
+	}
 
-// 	attributeGetCmd = &cobra.Command{
-// 		Use:   "get <id>",
-// 		Short: "Get an attribute",
-// 		Args:  cobra.ExactArgs(1),
-// 		Run: func(cmd *cobra.Command, args []string) {
-// 			h := cli.NewHandler(cmd)
-// 			defer h.Close()
+	// Create an attribute
+	attributesCreateCmd = &cobra.Command{
+		Use:   "create",
+		Short: "Create an attribute",
+		Run: func(cmd *cobra.Command, args []string) {
+			h := cli.NewHandler(cmd)
+			defer h.Close()
 
-// 			id, err := strconv.Atoi(args[0])
-// 			if err != nil {
-// 				cli.ExitWithError("Invalid ID", err)
-// 			}
+			flagHelper := cli.NewFlagHelper(cmd)
+			name := flagHelper.GetRequiredString("name")
+			rule := flagHelper.GetRequiredString("rule")
+			values := flagHelper.GetStringSlice("values", attrValues, cli.FlagHelperStringSliceOptions{})
+			namespace := flagHelper.GetRequiredString("namespace")
 
-// 			attr, err := h.GetAttribute(id)
-// 			if err != nil {
-// 				errMsg := fmt.Sprintf("Could not find attribute (%d)", id)
-// 				cli.ExitWithNotFoundError(errMsg, err)
-// 				cli.ExitWithError(errMsg, err)
-// 			}
+			if _, err := h.CreateAttribute(name, rule, values, namespace); err != nil {
+				cli.ExitWithError("Could not create attribute", err)
+			}
 
-// 			fmt.Println(cli.SuccessMessage("Attribute found"))
-// 			fmt.Println(
-// 				cli.NewTabular().
-// 					Rows([][]string{
-// 						{"Id", strconv.Itoa(int(attr.Id))},
-// 						{"Name", attr.Name},
-// 						{"Rule", attr.Rule},
-// 						{"Values", cli.CommaSeparated(attr.Values)},
-// 						{"Namespace", attr.Namespace},
-// 						{"Description", attr.Description},
-// 					}...).Render(),
-// 			)
-// 		},
-// 	}
+			fmt.Println(cli.SuccessMessage("Attribute created"))
+			fmt.Println(
+				cli.NewTabular().Rows([][]string{
+					{"Name", name},
+					{"Rule", rule},
+					{"Values", cli.CommaSeparated(values)},
+					{"Namespace", namespace},
+				}...).Render(),
+			)
+		},
+	}
 
-// 	// List attributes
-// 	attributesListCmd = &cobra.Command{
-// 		Use:   "list",
-// 		Short: "List attributes",
-// 		Run: func(cmd *cobra.Command, args []string) {
-// 			h := cli.NewHandler(cmd)
-// 			defer h.Close()
+	// Get an attribute
+	attributeGetCmd = &cobra.Command{
+		Use:   "get",
+		Short: "Get an attribute",
+		Run: func(cmd *cobra.Command, args []string) {
+			flagHelper := cli.NewFlagHelper(cmd)
+			id := flagHelper.GetRequiredString("id")
 
-// 			attrs, err := h.ListAttributes()
-// 			if err != nil {
-// 				cli.ExitWithError("Could not get attributes", err)
-// 			}
+			h := cli.NewHandler(cmd)
+			defer h.Close()
 
-// 			t := cli.NewTable()
-// 			t.Headers("Id", "Namespace", "Name", "Rule", "Values")
-// 			for _, attr := range attrs {
-// 				t.Row(
-// 					strconv.Itoa(int(attr.Id)),
-// 					attr.Namespace,
-// 					attr.Name,
-// 					attr.Rule,
-// 					cli.CommaSeparated(attr.Values),
-// 				)
-// 			}
-// 			fmt.Println(t.Render())
-// 		},
-// 	}
+			attr, err := h.GetAttribute(id)
+			if err != nil {
+				errMsg := fmt.Sprintf("Could not find attribute (%s)", id)
+				cli.ExitWithNotFoundError(errMsg, err)
+				cli.ExitWithError(errMsg, err)
+			}
 
-// 	// Create an attribute
-// 	attributesCreateCmd = &cobra.Command{
-// 		Use:   "create",
-// 		Short: "Create an attribute",
-// 		Run: func(cmd *cobra.Command, args []string) {
-// 			h := cli.NewHandler(cmd)
-// 			defer h.Close()
+			a := cli.GetSimpleAttribute(attr)
+			fmt.Println(cli.SuccessMessage("Attribute found"))
+			fmt.Println(
+				cli.NewTabular().
+					Rows([][]string{
+						{"Id", a.Id},
+						{"Name", a.Name},
+						{"Rule", a.Rule},
+						{"Values", cli.CommaSeparated(a.Values)},
+						{"Namespace", a.Namespace},
+					}...).Render(),
+			)
+		},
+	}
 
-// 			flagHelper := cli.NewFlagHelper(cmd)
-// 			name := flagHelper.GetRequiredString("name")
-// 			rule := flagHelper.GetRequiredString("rule")
-// 			values := flagHelper.GetStringSlice("values", attrValues, cli.FlagHelperStringSliceOptions{
-// 				Min: 1,
-// 			})
-// 			namespace := flagHelper.GetRequiredString("namespace")
-// 			description := flagHelper.GetRequiredString("description")
+	// List attributes
+	attributesListCmd = &cobra.Command{
+		Use:   "list",
+		Short: "List attributes",
+		Run: func(cmd *cobra.Command, args []string) {
+			h := cli.NewHandler(cmd)
+			defer h.Close()
 
-// 			if _, err := h.CreateAttribute(name, rule, values, namespace, description); err != nil {
-// 				cli.ExitWithError("Could not create attribute", err)
-// 			}
+			attrs, err := h.ListAttributes()
+			if err != nil {
+				cli.ExitWithError("Could not get attributes", err)
+			}
 
-// 			fmt.Println(cli.SuccessMessage("Attribute created"))
-// 			fmt.Println(
-// 				cli.NewTabular().Rows([][]string{
-// 					{"Name", name},
-// 					{"Rule", rule},
-// 					{"Values", cli.CommaSeparated(values)},
-// 					{"Namespace", namespace},
-// 					{"Description", description},
-// 				}...).Render(),
-// 			)
-// 		},
-// 	}
+			t := cli.NewTable()
+			t.Headers("Id", "Namespace", "Name", "Rule", "Values")
+			for _, attr := range attrs {
+				a := cli.GetSimpleAttribute(attr)
+				t.Row(
+					a.Id,
+					a.Namespace,
+					a.Name,
+					a.Rule,
+					cli.CommaSeparated(a.Values),
+				)
+			}
+			fmt.Println(t.Render())
+		},
+	}
 
-// 	attributesDeleteCmd = &cobra.Command{
-// 		Use:   "delete <id>",
-// 		Short: "Delete an attribute",
-// 		Args:  cobra.ExactArgs(1),
-// 		Run: func(cmd *cobra.Command, args []string) {
-// 			h := cli.NewHandler(cmd)
-// 			defer h.Close()
+	attributesDeleteCmd = &cobra.Command{
+		Use:   "delete",
+		Short: "Delete an attribute",
+		Run: func(cmd *cobra.Command, args []string) {
+			flagHelper := cli.NewFlagHelper(cmd)
+			id := flagHelper.GetRequiredString("id")
 
-// 			id, err := strconv.Atoi(args[0])
-// 			if err != nil {
-// 				fmt.Println(cli.ErrorMessage("Invalid ID", err))
-// 				os.Exit(1)
-// 			}
-// 			attr, err := h.GetAttribute(id)
-// 			if err != nil {
-// 				errMsg := fmt.Sprintf("Could not find attribute (%d)", id)
-// 				cli.ExitWithNotFoundError(errMsg, err)
-// 				cli.ExitWithError(errMsg, err)
-// 			}
+			h := cli.NewHandler(cmd)
+			defer h.Close()
 
-// 			cli.ConfirmDelete("attribute", attr.Fqn)
+			attr, err := h.GetAttribute(id)
+			if err != nil {
+				errMsg := fmt.Sprintf("Could not find attribute (%s)", id)
+				cli.ExitWithNotFoundError(errMsg, err)
+				cli.ExitWithError(errMsg, err)
+			}
 
-// 			if err := h.DeleteAttribute(id); err != nil {
-// 				errMsg := fmt.Sprintf("Could not delete attribute (%d)", id)
-// 				cli.ExitWithNotFoundError(errMsg, err)
-// 				cli.ExitWithError(errMsg, err)
-// 			}
+			cli.ConfirmDelete("attribute", attr.Name)
 
-// 			fmt.Println(cli.SuccessMessage("Attribute deleted"))
-// 			fmt.Println(
-// 				cli.NewTabular().
-// 					Rows([][]string{
-// 						{"Name", attr.Name},
-// 						{"Rule", attr.Rule},
-// 						{"Values", cli.CommaSeparated(attr.Values)},
-// 						{"Namespace", attr.Namespace},
-// 						{"Description", attr.Description},
-// 					}...).Render(),
-// 			)
-// 		},
-// 	}
+			attr, err = h.DeleteAttribute(id)
+			if err != nil {
+				errMsg := fmt.Sprintf("Could not delete attribute (%s)", id)
+				cli.ExitWithNotFoundError(errMsg, err)
+				cli.ExitWithError(errMsg, err)
+			}
 
-// 	// Update one attribute
-// 	attributeUpdateCmd = &cobra.Command{
-// 		Use:   "update",
-// 		Short: "Update an attribute",
-// 		Run: func(cmd *cobra.Command, args []string) {
-// 			h := cli.NewHandler(cmd)
-// 			defer h.Close()
+			a := cli.GetSimpleAttribute(attr)
+			fmt.Println(cli.SuccessMessage("Attribute deleted"))
+			fmt.Println(
+				cli.NewTabular().
+					Rows([][]string{
+						{"Name", a.Name},
+						{"Rule", a.Rule},
+						{"Values", cli.CommaSeparated(a.Values)},
+						{"Namespace", a.Namespace},
+					}...).Render(),
+			)
+		},
+	}
 
-// 			flagHelper := cli.NewFlagHelper(cmd)
+	// Update one attribute
+	attributeUpdateCmd = &cobra.Command{
+		Use:   "update",
+		Short: "Update an attribute",
+		Run: func(cmd *cobra.Command, args []string) {
+			h := cli.NewHandler(cmd)
+			defer h.Close()
 
-// 			id := flagHelper.GetRequiredInt32("id")
-// 			name := flagHelper.GetRequiredString("name")
-// 			rule := flagHelper.GetRequiredString("rule")
+			flagHelper := cli.NewFlagHelper(cmd)
+			id := flagHelper.GetRequiredString("id")
 
-// 			values := flagHelper.GetStringSlice("values", attrValues, cli.FlagHelperStringSliceOptions{
-// 				Min: 1,
-// 			})
+			if _, err := h.UpdateAttribute(id); err != nil {
+				cli.ExitWithError("Could not update attribute", err)
+			} else {
+				fmt.Println(cli.SuccessMessage(fmt.Sprintf("Attribute id: %s updated.", id)))
+			}
+		},
+	}
+)
 
-// 			groupBy := flagHelper.GetStringSlice("group-by", groupBy, cli.FlagHelperStringSliceOptions{
-// 				Min: 0,
-// 			})
+func init() {
+	rootCmd.AddCommand(attributesCmd)
 
-// 			resourceDependencies := flagHelper.GetStringSlice("resource-dependencies", resourceDependencies, cli.FlagHelperStringSliceOptions{
-// 				Min: 0,
-// 			})
+	// Create an attribute
+	attributesCmd.AddCommand(attributesCreateCmd)
+	attributesCreateCmd.Flags().StringP("name", "n", "", "Name of the attribute")
+	attributesCreateCmd.Flags().StringP("rule", "r", "", "Rule of the attribute")
+	attributesCreateCmd.Flags().StringSliceVarP(&attrValues, "values", "v", []string{}, "Values of the attribute")
+	attributesCreateCmd.Flags().StringP("namespace", "s", "", "Namespace of the attribute")
+	attributesCreateCmd.Flags().StringP("description", "d", "", "Description of the attribute")
 
-// 			resourceId := flagHelper.GetRequiredInt32("resource-id")
-// 			resourceVersion := flagHelper.GetRequiredInt32("resource-version")
-// 			resourceName := flagHelper.GetRequiredString("resource-name")
-// 			resourceNamespace := flagHelper.GetRequiredString("resource-namespace")
-// 			resourceDescription := flagHelper.GetRequiredString("resource-description")
+	// Get an attribute
+	attributesCmd.AddCommand(attributeGetCmd)
+	attributeGetCmd.Flags().StringP("id", "i", "", "Id of the attribute")
 
-// 			if _, err := h.UpdateAttribute(
-// 				id,
-// 				name,
-// 				rule,
-// 				values,
-// 				groupBy,
-// 				resourceId,
-// 				resourceVersion,
-// 				resourceName,
-// 				resourceNamespace,
-// 				resourceDescription,
-// 				resourceDependencies,
-// 			); err != nil {
-// 				cli.ExitWithError("Could not update attribute", err)
-// 				return
-// 			} else {
-// 				fmt.Println(cli.SuccessMessage(fmt.Sprintf("Attribute id: %d updated.", id)))
-// 			}
-// 		},
-// 	}
-// )
+	// List attributes
+	attributesCmd.AddCommand(attributesListCmd)
 
-// func init() {
-// 	rootCmd.AddCommand(attributesCmd)
+	// Update an attribute
+	attributesCmd.AddCommand(attributeUpdateCmd)
+	attributeUpdateCmd.Flags().StringP("id", "i", "", "Id of the attribute")
 
-// 	attributesCmd.AddCommand(attributeGetCmd)
-
-// 	attributesCmd.AddCommand(attributesListCmd)
-
-// 	attributesCmd.AddCommand(attributesCreateCmd)
-// 	attributesCreateCmd.Flags().StringP("name", "n", "", "Name of the attribute")
-// 	attributesCreateCmd.Flags().StringP("rule", "r", "", "Rule of the attribute")
-// 	attributesCreateCmd.Flags().StringSliceVarP(&attrValues, "values", "v", []string{}, "Values of the attribute")
-// 	attributesCreateCmd.Flags().StringP("namespace", "s", "", "Namespace of the attribute")
-// 	attributesCreateCmd.Flags().StringP("description", "d", "", "Description of the attribute")
-
-// 	attributesCmd.AddCommand(attributeUpdateCmd)
-// 	attributeUpdateCmd.Flags().Int32P("id", "i", 0, "Id of the attribute")
-// 	attributeUpdateCmd.Flags().StringP("name", "n", "", "Name of the attribute")
-// 	attributeUpdateCmd.Flags().StringP("rule", "r", "", "Rule of the attribute")
-// 	attributeUpdateCmd.Flags().StringSliceVarP(&attrValues, "values", "v", []string{}, "Values of the attribute")
-// 	attributeUpdateCmd.Flags().StringSliceVarP(&groupBy, "group-by", "g", []string{}, "GroupBy of the attribute")
-// 	attributeUpdateCmd.Flags().StringSliceVarP(&resourceDependencies, "resource-dependencies", "d", []string{}, "ResourceDependencies of the attribute definition descriptor")
-// 	attributeUpdateCmd.Flags().Int32P("resource-id", "I", 0, "ResourceId of the attribute definition descriptor")
-// 	attributeUpdateCmd.Flags().Int32P("resource-version", "V", 0, "ResourceVersion of the attribute definition descriptor")
-// 	attributeUpdateCmd.Flags().StringP("resource-name", "N", "", "ResourceName of the attribute definition descriptor")
-// 	attributeUpdateCmd.Flags().StringP("resource-namespace", "S", "", "ResourceNamespace of the attribute definition descriptor")
-// 	attributeUpdateCmd.Flags().StringP("resource-description", "D", "", "ResourceDescription of the attribute definition descriptor")
-
-// 	attributesCmd.AddCommand(attributesDeleteCmd)
-// }
+	// Delete an attribute
+	attributesCmd.AddCommand(attributesDeleteCmd)
+	attributesDeleteCmd.Flags().StringP("id", "i", "", "Id of the attribute")
+}
--- pkg/handlers/attribute.go
+++ pkg/handlers/attribute.go
@@ -1,216 +1,154 @@
 package handlers
 
-// import (
-// 	"fmt"
+import (
+	"fmt"
 
-// 	"github.com/opentdf/opentdf-v2-poc/sdk/attributes"
-// 	"github.com/opentdf/opentdf-v2-poc/sdk/common"
-// 	"github.com/opentdf/tructl/pkg/grpc"
-// )
+	"github.com/opentdf/opentdf-v2-poc/sdk/attributes"
+	"github.com/opentdf/opentdf-v2-poc/sdk/common"
+)
 
-// // TODO: Might be useful to map out the attribute rule definitions for help text in the CLI and TUI
+// TODO: Might be useful to map out the attribute rule definitions for help text in the CLI and TUI
 
-// const (
-// 	AttributeRuleAllOf       = "ALL_OF"
-// 	AttributeRuleAnyOf       = "ANY_OF"
-// 	AttributeRuleHierarchy   = "HIERARCHY"
-// 	AttributeRuleUnspecified = "UNSPECIFIED"
-// )
+const (
+	AttributeRuleAllOf     = "ALL_OF"
+	AttributeRuleAnyOf     = "ANY_OF"
+	AttributeRuleHierarchy = "HIERARCHY"
+)
 
-// type Attribute struct {
-// 	Id          int32
-// 	Name        string
-// 	Rule        string
-// 	Values      []string
-// 	Namespace   string
-// 	Description string
-// 	Fqn         string
-// }
+type CreateAttributeError struct {
+	ValueErrors map[string]error
 
-// func (h Handler) GetAttribute(id int) (Attribute, error) {
-// 	resp, err := h.sdk.Attributes.GetAttribute(h.ctx, &attributes.GetAttributeRequest{
-// 		Id: int32(id),
-// 	})
-// 	if err != nil {
-// 		return Attribute{}, err
-// 	}
+	Err error
+}
 
-// 	values := []string{}
-// 	for _, v := range resp.Definition.Values {
-// 		values = append(values, v.Value)
-// 	}
+func (e *CreateAttributeError) Error() string {
+	if e.ValueErrors != nil {
+		return "Error creating attribute with values" + fmt.Sprintf("%v", e.ValueErrors)
+	}
 
-// 	return Attribute{
-// 		Id:          resp.Definition.Descriptor_.Id,
-// 		Name:        resp.Definition.Name,
-// 		Rule:        GetAttributeRuleFromAttributeType(resp.Definition.Rule),
-// 		Values:      values,
-// 		Namespace:   resp.Definition.Descriptor_.Namespace,
-// 		Description: resp.Definition.Descriptor_.Description,
-// 		Fqn:         GetAttributeFqn(resp.Definition.Descriptor_.Namespace, resp.Definition.Name),
-// 	}, nil
-// }
+	return "Error creating attribute"
+}
 
-// func (h Handler) ListAttributes() ([]Attribute, error) {
-// 	resp, err := h.sdk.Attributes.ListAttributes(h.ctx, &attributes.ListAttributesRequest{})
-// 	if err != nil {
-// 		return nil, err
-// 	}
+func (h Handler) GetAttribute(id string) (*attributes.Attribute, error) {
+	resp, err := h.sdk.Attributes.GetAttribute(h.ctx, &attributes.GetAttributeRequest{
+		Id: id,
+	})
+	if err != nil {
+		return nil, err
+	}
 
-// 	var attrs []Attribute
-// 	for _, attr := range resp.Definitions {
-// 		values := []string{}
-// 		for _, v := range attr.Values {
-// 			values = append(values, v.Value)
-// 		}
-// 		attrs = append(attrs, Attribute{
-// 			Id:          attr.Descriptor_.Id,
-// 			Name:        attr.Name,
-// 			Rule:        GetAttributeRuleFromAttributeType(attr.Rule),
-// 			Values:      values,
-// 			Namespace:   attr.Descriptor_.Namespace,
-// 			Description: attr.Descriptor_.Description,
-// 		})
-// 	}
+	return resp.Attribute, nil
+}
 
-// 	return attrs, err
-// }
+func (h Handler) ListAttributes() ([]*attributes.Attribute, error) {
+	resp, err := h.sdk.Attributes.ListAttributes(h.ctx, &attributes.ListAttributesRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Attributes, err
+}
 
-// func (h Handler) CreateAttribute(name string, rule string, values []string, namespace string, description string) (Attribute, error) {
-// 	var attrValues []*attributes.AttributeDefinitionValue
-// 	for _, v := range values {
-// 		if v != "" {
-// 			attrValues = append(attrValues, &attributes.AttributeDefinitionValue{Value: v})
-// 		}
-// 	}
+func (h Handler) CreateAttribute(name string, rule string, values []string, namespace string) (*attributes.Attribute, error) {
+	r, err := GetAttributeRuleFromReadableString(rule)
+	if err != nil {
+		return nil, err
+	}
 
-// 	_, err := h.sdk.Attributes.CreateAttribute(h.ctx, &attributes.CreateAttributeRequest{
-// 		Definition: &attributes.AttributeDefinition{
-// 			Name:   name,
-// 			Rule:   GetAttributeRuleFromReadableString(rule),
-// 			Values: attrValues,
-// 			Descriptor_: &common.ResourceDescriptor{
-// 				Namespace:   namespace,
-// 				Name:        name,
-// 				Type:        common.PolicyResourceType_POLICY_RESOURCE_TYPE_ATTRIBUTE_DEFINITION,
-// 				Description: description,
-// 			},
-// 		},
-// 	})
-// 	if err != nil {
-// 		return Attribute{}, err
-// 	}
+	attrReq := &attributes.CreateAttributeRequest{
+		Attribute: &attributes.AttributeCreateUpdate{
+			NamespaceId: namespace,
+			Name:        name,
+			Rule:        r,
+		},
+	}
 
-// 	return Attribute{
-// 		Name:        name,
-// 		Rule:        rule,
-// 		Values:      values,
-// 		Namespace:   namespace,
-// 		Description: description,
-// 	}, nil
-// }
+	resp, err := h.sdk.Attributes.CreateAttribute(h.ctx, attrReq)
+	if err != nil {
+		return nil, err
+	}
 
-// func (h *Handler) UpdateAttribute(
-// 	id int32,
-// 	name string,
-// 	rule string,
-// 	values []string,
-// 	groupBy []string,
-// 	resourceId int32,
-// 	resourceVersion int32,
-// 	resourceName string,
-// 	resourceNamespace string,
-// 	resourceDescription string,
-// 	resourceDependencies []string,
-// ) (*attributes.UpdateAttributeResponse, error) {
-// 	var attrValues []*attributes.AttributeDefinitionValue
-// 	for _, v := range values {
-// 		if v != "" {
-// 			attrValues = append(attrValues, &attributes.AttributeDefinitionValue{Value: v})
-// 		}
-// 	}
+	// short-circuit if there are no values
+	if len(values) == 0 {
+		return resp.Attribute, nil
+	}
 
-// 	var attrGroupBy []*attributes.AttributeDefinitionValue
-// 	for _, v := range groupBy {
-// 		if v != "" {
-// 			attrGroupBy = append(attrGroupBy, &attributes.AttributeDefinitionValue{Value: v})
-// 		}
-// 	}
+	// create attribute values
+	attr := resp.Attribute
+	attrValues := make([]*attributes.Value, 0, len(values))
+	valueErrors := make(map[string]error)
+	for _, value := range values {
+		v, err := h.CreateAttributeValue(attr.Id, value)
+		if err != nil {
+			valueErrors[value] = err
+		}
+		attrValues = append(attrValues, v)
+	}
 
-// 	var dependencies []*common.ResourceDependency
-// 	for _, v := range resourceDependencies {
-// 		if v != "" {
-// 			dependencies = append(dependencies, &common.ResourceDependency{Namespace: v})
-// 		}
-// 	}
+	// return the attribute and any errors
+	return &attributes.Attribute{
+		Id:        attr.Id,
+		Name:      attr.Name,
+		Rule:      attr.Rule,
+		Values:    attrValues,
+		Namespace: attr.Namespace,
+	}, &CreateAttributeError{ValueErrors: valueErrors}
+}
 
-// 	return h.sdk.Attributes.UpdateAttribute(grpc.Context, &attributes.UpdateAttributeRequest{
-// 		Id: id,
-// 		Definition: &attributes.AttributeDefinition{
-// 			Name:    name,
-// 			Rule:    GetAttributeRuleFromReadableString(rule),
-// 			Values:  attrValues,
-// 			GroupBy: attrGroupBy,
-// 			Descriptor_: &common.ResourceDescriptor{
-// 				Type:         common.PolicyResourceType_POLICY_RESOURCE_TYPE_ATTRIBUTE_DEFINITION,
-// 				Id:           resourceId,
-// 				Version:      resourceVersion,
-// 				Name:         resourceName,
-// 				Namespace:    resourceNamespace,
-// 				Fqn:          GetAttributeFqn(resourceNamespace, resourceName),
-// 				Description:  resourceDescription,
-// 				Dependencies: dependencies,
-// 			},
-// 		},
-// 	})
-// }
+func (h *Handler) UpdateAttribute(
+	id string,
+	fns ...func(*common.MetadataMutable) *common.MetadataMutable,
+) (*attributes.UpdateAttributeResponse, error) {
+	return h.sdk.Attributes.UpdateAttribute(h.ctx, &attributes.UpdateAttributeRequest{
+		Id: id,
+		Attribute: &attributes.AttributeCreateUpdate{
+			Metadata: buildMetadata(&common.MetadataMutable{}, fns...),
+		},
+	})
+}
 
-// func (h Handler) DeleteAttribute(id int) error {
-// 	_, err := h.sdk.Attributes.DeleteAttribute(h.ctx, &attributes.DeleteAttributeRequest{
-// 		Id: int32(id),
-// 	})
+func (h Handler) DeleteAttribute(id string) (*attributes.Attribute, error) {
+	resp, err := h.sdk.Attributes.DeleteAttribute(h.ctx, &attributes.DeleteAttributeRequest{
+		Id: id,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Attribute, err
+}
 
-// 	return err
-// }
+func GetAttributeFqn(namespace string, name string) string {
+	return fmt.Sprintf("https://%s/attr/%s", namespace, name)
+}
 
-// func GetAttributeFqn(namespace string, name string) string {
-// 	return fmt.Sprintf("https://%s/attr/%s", namespace, name)
-// }
+func GetAttributeRuleOptions() []string {
+	return []string{
+		AttributeRuleAllOf,
+		AttributeRuleAnyOf,
+		AttributeRuleHierarchy,
+	}
+}
 
-// func GetAttributeRuleOptions() []string {
-// 	return []string{
-// 		AttributeRuleAllOf,
-// 		AttributeRuleAnyOf,
-// 		AttributeRuleHierarchy,
-// 		AttributeRuleUnspecified,
-// 	}
-// }
+func GetAttributeRuleFromAttributeType(rule attributes.AttributeRuleTypeEnum) string {
+	switch rule {
+	case attributes.AttributeRuleTypeEnum_ATTRIBUTE_RULE_TYPE_ENUM_ALL_OF:
+		return AttributeRuleAllOf
+	case attributes.AttributeRuleTypeEnum_ATTRIBUTE_RULE_TYPE_ENUM_ANY_OF:
+		return AttributeRuleAnyOf
+	case attributes.AttributeRuleTypeEnum_ATTRIBUTE_RULE_TYPE_ENUM_HIERARCHY:
+		return AttributeRuleHierarchy
+	default:
+		return ""
+	}
+}
 
-// func GetAttributeRuleFromAttributeType(rule attributes.AttributeDefinition_AttributeRuleType) string {
-// 	switch rule {
-// 	case attributes.AttributeDefinition_ATTRIBUTE_RULE_TYPE_ALL_OF:
-// 		return AttributeRuleAllOf
-// 	case attributes.AttributeDefinition_ATTRIBUTE_RULE_TYPE_ANY_OF:
-// 		return AttributeRuleAnyOf
-// 	case attributes.AttributeDefinition_ATTRIBUTE_RULE_TYPE_HIERARCHICAL:
-// 		return AttributeRuleHierarchy
-// 	case attributes.AttributeDefinition_ATTRIBUTE_RULE_TYPE_UNSPECIFIED:
-// 		return AttributeRuleUnspecified
-// 	default:
-// 		return ""
-// 	}
-// }
-
-// func GetAttributeRuleFromReadableString(rule string) attributes.AttributeDefinition_AttributeRuleType {
-// 	switch rule {
-// 	case AttributeRuleAllOf:
-// 		return attributes.AttributeDefinition_ATTRIBUTE_RULE_TYPE_ALL_OF
-// 	case AttributeRuleAnyOf:
-// 		return attributes.AttributeDefinition_ATTRIBUTE_RULE_TYPE_ANY_OF
-// 	case AttributeRuleHierarchy:
-// 		return attributes.AttributeDefinition_ATTRIBUTE_RULE_TYPE_HIERARCHICAL
-// 	case AttributeRuleUnspecified:
-// 		return attributes.AttributeDefinition_ATTRIBUTE_RULE_TYPE_UNSPECIFIED
-// 	}
-// 	return attributes.AttributeDefinition_ATTRIBUTE_RULE_TYPE_UNSPECIFIED
-// }
+func GetAttributeRuleFromReadableString(rule string) (attributes.AttributeRuleTypeEnum, error) {
+	switch rule {
+	case AttributeRuleAllOf:
+		return attributes.AttributeRuleTypeEnum_ATTRIBUTE_RULE_TYPE_ENUM_ALL_OF, nil
+	case AttributeRuleAnyOf:
+		return attributes.AttributeRuleTypeEnum_ATTRIBUTE_RULE_TYPE_ENUM_ANY_OF, nil
+	case AttributeRuleHierarchy:
+		return attributes.AttributeRuleTypeEnum_ATTRIBUTE_RULE_TYPE_ENUM_HIERARCHY, nil
+	}
+	return 0, fmt.Errorf("invalid attribute rule: %s", rule)
+}
