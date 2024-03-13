6b9c5c5e9b56d1c915e5a529718266c28cf64fe0
jakedoublev <jake.vanvorhis@virtru.com>
2024-03-13 17:01:38 -0400

    casing

--- cmd/policy-subject_mappings.go
+++ cmd/policy-subject_mappings.go
@@ -13,19 +13,19 @@ import "fmt"
 // )
 
 // var (
-// 	policy_subjectMappingsCmds = []string{
+// 	policy_subject_mappingsCmds = []string{
 // 		policy_subjectMappingCreateCmd.Use,
 // 		policy_subjectMappingGetCmd.Use,
-// 		policy_subjectMappingsListCmd.Use,
+// 		policy_subject_mappingsListCmd.Use,
 // 		policy_subjectMappingUpdateCmd.Use,
 // 		policy_subjectMappingDeleteCmd.Use,
 // 	}
 
 // 	subjectValues []string
 
-// 	policy_subjectMappingsCmd = &cobra.Command{
+// 	policy_subject_mappingsCmd = &cobra.Command{
 // 		Use:   "subject-mappings",
-// 		Short: "Manage subject mappings [" + strings.Join(policy_subjectMappingsCmds, ", ") + "]",
+// 		Short: "Manage subject mappings [" + strings.Join(policy_subject_mappingsCmds, ", ") + "]",
 // 		Long: `
 // Subject Mappings - commands to manage relationships between subjects (PEs, NPEs, etc) and attributes.
 
@@ -76,7 +76,7 @@ import "fmt"
 // 		},
 // 	}
 
-// 	policy_subjectMappingsListCmd = &cobra.Command{
+// 	policy_subject_mappingsListCmd = &cobra.Command{
 // 		Use:   "list",
 // 		Short: "List subject mappings",
 // 		Run: func(cmd *cobra.Command, args []string) {
@@ -226,21 +226,21 @@ import "fmt"
 // )
 
 // func init() {
-// 	policyCmd.AddCommand(policy_subjectMappingsCmd)
+// 	policyCmd.AddCommand(policy_subject_mappingsCmd)
 
-// 	policy_subjectMappingsCmd.AddCommand(policy_subjectMappingGetCmd)
+// 	policy_subject_mappingsCmd.AddCommand(policy_subjectMappingGetCmd)
 // 	policy_subjectMappingGetCmd.Flags().StringP("id", "i", "", "Id of the subject mapping")
 
-// 	policy_subjectMappingsCmd.AddCommand(policy_subjectMappingsListCmd)
+// 	policy_subject_mappingsCmd.AddCommand(policy_subject_mappingsListCmd)
 
-// 	policy_subjectMappingsCmd.AddCommand(policy_subjectMappingCreateCmd)
+// 	policy_subject_mappingsCmd.AddCommand(policy_subjectMappingCreateCmd)
 // 	policy_subjectMappingCreateCmd.Flags().StringP("attribute-value-id", "a", "", "Id of the attribute value")
 // 	policy_subjectMappingCreateCmd.Flags().StringP("subject-attribute", "s", "", "Subject attribute")
 // 	policy_subjectMappingCreateCmd.Flags().StringSliceVarP(&subjectValues, "subject-values", "v", []string{}, "Subject values")
 // 	policy_subjectMappingCreateCmd.Flags().StringP("operator", "o", "", "Operator")
 // 	policy_subjectMappingCreateCmd.Flags().StringP("metadata", "m", "", "Metadata (optional): labels and description")
 
-// 	policy_subjectMappingsCmd.AddCommand(policy_subjectMappingUpdateCmd)
+// 	policy_subject_mappingsCmd.AddCommand(policy_subjectMappingUpdateCmd)
 // 	policy_subjectMappingUpdateCmd.Flags().StringP("id", "i", "", "Id of the subject mapping")
 // 	policy_subjectMappingUpdateCmd.Flags().StringP("attribute-value-id", "a", "", "Id of the attribute value")
 // 	policy_subjectMappingUpdateCmd.Flags().StringP("subject-attribute", "s", "", "Subject attribute")
@@ -248,7 +248,7 @@ import "fmt"
 // 	policy_subjectMappingUpdateCmd.Flags().StringP("operator", "o", "", "Operator: [IN, NOT_IN]")
 // 	policy_subjectMappingUpdateCmd.Flags().StringP("metadata", "m", "", "Metadata (optional): labels and description")
 
-// 	policy_subjectMappingsCmd.AddCommand(policy_subjectMappingDeleteCmd)
+// 	policy_subject_mappingsCmd.AddCommand(policy_subjectMappingDeleteCmd)
 // 	policy_subjectMappingDeleteCmd.Flags().StringP("id", "i", "", "Id of the subject mapping")
 // }
 
