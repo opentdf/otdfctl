c97bf34673569dde207d46d9f0e5cde7ffc5a6dc
Ryan Schumacher <jschumacher@virtru.com>
2024-04-09 10:36:15 -0500

    refactor resource mappings

--- cmd/policy-resourceMappings.go
+++ cmd/policy-resourceMappings.go
@@ -14,208 +14,182 @@ import (
 
 var (
 	policy_resource_mappingsTerms []string
-
-	//#region Resource Mappings
-	policy_resource_mappingsCmd = &cobra.Command{
-		Use:     man.Docs.GetDoc("policy/resource-mappings").Use,
-		Aliases: man.Docs.GetDoc("policy/resource-mappings").Aliases,
-		Short: man.Docs.GetDoc("policy/resource-mappings").GetShort([]string{
-			policy_resource_mappingsCreateCmd.Use,
-			policy_resource_mappingsGetCmd.Use,
-			policy_resource_mappingsListCmd.Use,
-			policy_resource_mappingsUpdateCmd.Use,
-			policy_resource_mappingsDeleteCmd.Use,
-		}),
-		Long: man.Docs.GetDoc("policy/resource-mappings").Long,
-	}
-	//#endregion
-
-	//#region Resource Mapping Create
-	policy_resource_mappingsCreateCmd = &cobra.Command{
-		Use:   man.Docs.GetDoc("policy/resource-mappings/create").Use,
-		Short: man.Docs.GetDoc("policy/resource-mappings/create").Short,
-		Run: func(cmd *cobra.Command, args []string) {
-			h := cli.NewHandler(cmd)
-			defer h.Close()
-
-			flagHelper := cli.NewFlagHelper(cmd)
-			attrId := flagHelper.GetRequiredString("attribute-value-id")
-			terms := flagHelper.GetStringSlice("terms", policy_resource_mappingsTerms, cli.FlagHelperStringSliceOptions{
-				Min: 1,
-			})
-			metadataLabels := flagHelper.GetStringSlice("label", metadataLabels, cli.FlagHelperStringSliceOptions{Min: 0})
-
-			resourceMapping, err := h.CreateResourceMapping(attrId, terms, getMetadataMutable(metadataLabels))
-			if err != nil {
-				cli.ExitWithError("Failed to create resource mapping", err)
-			}
-
-			t := cli.NewTabular().Rows([][]string{
-				{"Id", resourceMapping.Id},
-				{"Attribute Value Id", resourceMapping.AttributeValue.Id},
-				{"Attribute Value", resourceMapping.AttributeValue.Value},
-				{"Terms", strings.Join(resourceMapping.Terms, ", ")},
-			}...)
-			HandleSuccess(cmd, resourceMapping.Id, t, resourceMapping)
-		},
-	}
-	//#endregion
-
-	policy_resource_mappingsGetCmd = &cobra.Command{
-		Use:   man.Docs.GetDoc("policy/resource-mappings/get").Use,
-		Short: man.Docs.GetDoc("policy/resource-mappings/get").Short,
-		Run: func(cmd *cobra.Command, args []string) {
-			h := cli.NewHandler(cmd)
-			defer h.Close()
-
-			flagHelper := cli.NewFlagHelper(cmd)
-			id := flagHelper.GetRequiredString("id")
-
-			resourceMapping, err := h.GetResourceMapping(id)
-			if err != nil {
-				cli.ExitWithError(fmt.Sprintf("Failed to get resource mapping (%s)", id), err)
-			}
-
-			t := cli.NewTabular().Rows([][]string{
-				{"Id", resourceMapping.Id},
-				{"Attribute Value Id", resourceMapping.AttributeValue.Id},
-				{"Attribute Value", resourceMapping.AttributeValue.Value},
-				{"Terms", strings.Join(resourceMapping.Terms, ", ")},
-			}...)
-			HandleSuccess(cmd, resourceMapping.Id, t, resourceMapping)
-		},
-	}
-
-	policy_resource_mappingsListCmd = &cobra.Command{
-		Use:   man.Docs.GetDoc("policy/resource-mappings/list").Use,
-		Short: man.Docs.GetDoc("policy/resource-mappings/list").Short,
-		Run: func(cmd *cobra.Command, args []string) {
-			h := cli.NewHandler(cmd)
-			defer h.Close()
-
-			rmList, err := h.ListResourceMappings()
-			if err != nil {
-				cli.ExitWithError("Failed to list resource mappings", err)
-			}
-
-			t := cli.NewTable()
-			t.Headers("Id", "Attribute Value Id", "Attribute Value", "Terms")
-			for _, resourceMapping := range rmList {
-				t.Row(resourceMapping.Id, resourceMapping.AttributeValue.Id, resourceMapping.AttributeValue.Value, strings.Join(resourceMapping.Terms, ", "))
-			}
-			HandleSuccess(cmd, "", t, rmList)
-		},
-	}
-
-	policy_resource_mappingsUpdateCmd = &cobra.Command{
-		Use:   man.Docs.GetDoc("policy/resource-mappings/update").Use,
-		Short: man.Docs.GetDoc("policy/resource-mappings/update").Short,
-		Run: func(cmd *cobra.Command, args []string) {
-			h := cli.NewHandler(cmd)
-			defer h.Close()
-
-			flagHelper := cli.NewFlagHelper(cmd)
-			id := flagHelper.GetRequiredString("id")
-			attrValueId := flagHelper.GetOptionalString("attribute-value-id")
-			terms := flagHelper.GetStringSlice("terms", policy_resource_mappingsTerms, cli.FlagHelperStringSliceOptions{})
-			labels := flagHelper.GetStringSlice("label", metadataLabels, cli.FlagHelperStringSliceOptions{Min: 0})
-
-			resourceMapping, err := h.UpdateResourceMapping(id, attrValueId, terms, getMetadataMutable(labels), getMetadataUpdateBehavior())
-			if err != nil {
-				cli.ExitWithError(fmt.Sprintf("Failed to update resource mapping (%s)", id), err)
-			}
-
-			t := cli.NewTabular().Rows([][]string{
-				{"Id", resourceMapping.Id},
-				{"Attribute Value Id", resourceMapping.AttributeValue.Id},
-				{"Attribute Value", resourceMapping.AttributeValue.Value},
-				{"Terms", strings.Join(resourceMapping.Terms, ", ")},
-			}...)
-			HandleSuccess(cmd, resourceMapping.Id, t, resourceMapping)
-		},
-	}
-
-	policy_resource_mappingsDeleteCmd = &cobra.Command{
-		Use:   man.Docs.GetDoc("policy/resource-mappings/delete").Use,
-		Short: man.Docs.GetDoc("policy/resource-mappings/delete").Short,
-		Run: func(cmd *cobra.Command, args []string) {
-			h := cli.NewHandler(cmd)
-			defer h.Close()
-
-			flagHelper := cli.NewFlagHelper(cmd)
-			id := flagHelper.GetRequiredString("id")
-
-			cli.ConfirmAction(cli.ActionDelete, "resource-mapping", id)
-
-			resourceMapping, err := h.DeleteResourceMapping(id)
-			if err != nil {
-				cli.ExitWithError(fmt.Sprintf("Failed to delete resource mapping (%s)", id), err)
-			}
-
-			t := cli.NewTabular().Rows([][]string{
-				{"Id", resourceMapping.Id},
-				{"Attribute Value Id", resourceMapping.AttributeValue.Id},
-				{"Attribute Value", resourceMapping.AttributeValue.Value},
-				{"Terms", strings.Join(resourceMapping.Terms, ", ")},
-			}...)
-			HandleSuccess(cmd, resourceMapping.Id, t, resourceMapping)
-		},
-	}
+	policy_resourceMappingsCmd    *cobra.Command
 )
 
-func init() {
-	policyCmd.AddCommand(policy_resource_mappingsCmd)
+func policy_createResourceMapping(cmd *cobra.Command, args []string) {
+	h := cli.NewHandler(cmd)
+	defer h.Close()
 
-	createDoc := man.Docs.GetDoc("policy/resource-mappings/create")
-	policy_resource_mappingsCmd.AddCommand(policy_resource_mappingsCreateCmd)
-	policy_resource_mappingsCreateCmd.Flags().String(
+	flagHelper := cli.NewFlagHelper(cmd)
+	attrId := flagHelper.GetRequiredString("attribute-value-id")
+	terms := flagHelper.GetStringSlice("terms", policy_resource_mappingsTerms, cli.FlagHelperStringSliceOptions{
+		Min: 1,
+	})
+	metadataLabels := flagHelper.GetStringSlice("label", metadataLabels, cli.FlagHelperStringSliceOptions{Min: 0})
+
+	resourceMapping, err := h.CreateResourceMapping(attrId, terms, getMetadataMutable(metadataLabels))
+	if err != nil {
+		cli.ExitWithError("Failed to create resource mapping", err)
+	}
+
+	t := cli.NewTabular().Rows([][]string{
+		{"Id", resourceMapping.Id},
+		{"Attribute Value Id", resourceMapping.AttributeValue.Id},
+		{"Attribute Value", resourceMapping.AttributeValue.Value},
+		{"Terms", strings.Join(resourceMapping.Terms, ", ")},
+	}...)
+	HandleSuccess(cmd, resourceMapping.Id, t, resourceMapping)
+}
+
+func policy_getResourceMapping(cmd *cobra.Command, args []string) {
+	h := cli.NewHandler(cmd)
+	defer h.Close()
+
+	flagHelper := cli.NewFlagHelper(cmd)
+	id := flagHelper.GetRequiredString("id")
+
+	resourceMapping, err := h.GetResourceMapping(id)
+	if err != nil {
+		cli.ExitWithError(fmt.Sprintf("Failed to get resource mapping (%s)", id), err)
+	}
+
+	t := cli.NewTabular().Rows([][]string{
+		{"Id", resourceMapping.Id},
+		{"Attribute Value Id", resourceMapping.AttributeValue.Id},
+		{"Attribute Value", resourceMapping.AttributeValue.Value},
+		{"Terms", strings.Join(resourceMapping.Terms, ", ")},
+	}...)
+	HandleSuccess(cmd, resourceMapping.Id, t, resourceMapping)
+}
+
+func policy_listResourceMappings(cmd *cobra.Command, args []string) {
+	h := cli.NewHandler(cmd)
+	defer h.Close()
+
+	rmList, err := h.ListResourceMappings()
+	if err != nil {
+		cli.ExitWithError("Failed to list resource mappings", err)
+	}
+
+	t := cli.NewTable()
+	t.Headers("Id", "Attribute Value Id", "Attribute Value", "Terms")
+	for _, resourceMapping := range rmList {
+		t.Row(resourceMapping.Id, resourceMapping.AttributeValue.Id, resourceMapping.AttributeValue.Value, strings.Join(resourceMapping.Terms, ", "))
+	}
+	HandleSuccess(cmd, "", t, rmList)
+}
+
+func policy_updateResourceMapping(cmd *cobra.Command, args []string) {
+	h := cli.NewHandler(cmd)
+	defer h.Close()
+
+	flagHelper := cli.NewFlagHelper(cmd)
+	id := flagHelper.GetRequiredString("id")
+	attrValueId := flagHelper.GetOptionalString("attribute-value-id")
+	terms := flagHelper.GetStringSlice("terms", policy_resource_mappingsTerms, cli.FlagHelperStringSliceOptions{})
+	labels := flagHelper.GetStringSlice("label", metadataLabels, cli.FlagHelperStringSliceOptions{Min: 0})
+
+	resourceMapping, err := h.UpdateResourceMapping(id, attrValueId, terms, getMetadataMutable(labels), getMetadataUpdateBehavior())
+	if err != nil {
+		cli.ExitWithError(fmt.Sprintf("Failed to update resource mapping (%s)", id), err)
+	}
+
+	t := cli.NewTabular().Rows([][]string{
+		{"Id", resourceMapping.Id},
+		{"Attribute Value Id", resourceMapping.AttributeValue.Id},
+		{"Attribute Value", resourceMapping.AttributeValue.Value},
+		{"Terms", strings.Join(resourceMapping.Terms, ", ")},
+	}...)
+	HandleSuccess(cmd, resourceMapping.Id, t, resourceMapping)
+}
+
+func policy_deleteResourceMapping(cmd *cobra.Command, args []string) {
+	h := cli.NewHandler(cmd)
+	defer h.Close()
+
+	flagHelper := cli.NewFlagHelper(cmd)
+	id := flagHelper.GetRequiredString("id")
+
+	cli.ConfirmAction(cli.ActionDelete, "resource-mapping", id)
+
+	resourceMapping, err := h.DeleteResourceMapping(id)
+	if err != nil {
+		cli.ExitWithError(fmt.Sprintf("Failed to delete resource mapping (%s)", id), err)
+	}
+
+	t := cli.NewTabular().Rows([][]string{
+		{"Id", resourceMapping.Id},
+		{"Attribute Value Id", resourceMapping.AttributeValue.Id},
+		{"Attribute Value", resourceMapping.AttributeValue.Value},
+		{"Terms", strings.Join(resourceMapping.Terms, ", ")},
+	}...)
+	HandleSuccess(cmd, resourceMapping.Id, t, resourceMapping)
+}
+
+func init() {
+	createDoc := man.Docs.GetCommand("policy/resource-mappings/create",
+		man.WithRun(policy_createResourceMapping),
+	)
+	createDoc.Flags().String(
 		createDoc.GetDocFlag("attribute-value-id").Name,
 		createDoc.GetDocFlag("attribute-value-id").Default,
 		createDoc.GetDocFlag("attribute-value-id").Description,
 	)
-	policy_resource_mappingsCreateCmd.Flags().StringSliceVar(
+	createDoc.Flags().StringSliceVar(
 		&policy_resource_mappingsTerms,
 		createDoc.GetDocFlag("terms").Name,
 		[]string{},
 		createDoc.GetDocFlag("terms").Description,
 	)
-	injectLabelFlags(policy_resource_mappingsCreateCmd, false)
+	injectLabelFlags(&createDoc.Command, false)
 
-	getDoc := man.Docs.GetDoc("policy/resource-mappings/get")
-	policy_resource_mappingsCmd.AddCommand(policy_resource_mappingsGetCmd)
-	policy_resource_mappingsGetCmd.Flags().String(
+	getDoc := man.Docs.GetCommand("policy/resource-mappings/get",
+		man.WithRun(policy_getResourceMapping),
+	)
+	getDoc.Flags().String(
 		getDoc.GetDocFlag("id").Name,
 		getDoc.GetDocFlag("id").Default,
 		getDoc.GetDocFlag("id").Description,
 	)
 
-	policy_resource_mappingsCmd.AddCommand(policy_resource_mappingsListCmd)
+	listDoc := man.Docs.GetCommand("policy/resource-mappings/list",
+		man.WithRun(policy_listResourceMappings),
+	)
 
-	updateDoc := man.Docs.GetDoc("policy/resource-mappings/update")
-	policy_resource_mappingsCmd.AddCommand(policy_resource_mappingsUpdateCmd)
-	policy_resource_mappingsUpdateCmd.Flags().String(
+	updateDoc := man.Docs.GetCommand("policy/resource-mappings/update",
+		man.WithRun(policy_updateResourceMapping),
+	)
+	updateDoc.Flags().String(
 		updateDoc.GetDocFlag("id").Name,
 		updateDoc.GetDocFlag("id").Default,
 		updateDoc.GetDocFlag("id").Description,
 	)
-	policy_resource_mappingsUpdateCmd.Flags().String(
+	updateDoc.Flags().String(
 		updateDoc.GetDocFlag("attribute-value-id").Name,
 		updateDoc.GetDocFlag("attribute-value-id").Default,
 		updateDoc.GetDocFlag("attribute-value-id").Description,
 	)
-	policy_resource_mappingsUpdateCmd.Flags().StringSliceVar(
+	updateDoc.Flags().StringSliceVar(
 		&policy_resource_mappingsTerms,
 		updateDoc.GetDocFlag("terms").Name,
 		[]string{},
 		updateDoc.GetDocFlag("terms").Description,
 	)
-	injectLabelFlags(policy_resource_mappingsUpdateCmd, true)
+	injectLabelFlags(&updateDoc.Command, true)
 
-	deleteDoc := man.Docs.GetDoc("policy/resource-mappings/delete")
-	policy_resource_mappingsCmd.AddCommand(policy_resource_mappingsDeleteCmd)
-	policy_resource_mappingsDeleteCmd.Flags().String(
+	deleteDoc := man.Docs.GetCommand("policy/resource-mappings/delete",
+		man.WithRun(policy_deleteResourceMapping),
+	)
+	deleteDoc.Flags().String(
 		deleteDoc.GetDocFlag("id").Name,
 		deleteDoc.GetDocFlag("id").Default,
 		deleteDoc.GetDocFlag("id").Description,
 	)
+
+	doc := man.Docs.GetCommand("policy/resource-mappings",
+		man.WithSubcommands(createDoc, getDoc, listDoc, updateDoc, deleteDoc),
+	)
+	policy_resourceMappingsCmd = &doc.Command
+	policyCmd.AddCommand(policy_resourceMappingsCmd)
 }
