37a3ef313f68e331441b2a25e3639bdc890e8609
Krish Suchak <suchak.krish@gmail.com>
2024-05-15 11:21:41 -0400

    clean up labelUpdate.go

--- tui/labelUpdate.go
+++ tui/labelUpdate.go
@@ -15,7 +15,6 @@ type LabelUpdate struct {
 }
 
 func InitLabelUpdate(label LabelItem, attr *policy.Attribute, sdk handlers.Handler) LabelUpdate {
-	// label := attr.Metadata.Labels[labelIdx]
 	return LabelUpdate{
 		label:  label,
 		update: InitUpdate([]string{"Key", "Value"}, []string{label.title, label.description}),
@@ -33,21 +32,17 @@ func (m LabelUpdate) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "enter":
-			// return InitLabelList(m.attr, m.sdk)
 			if m.update.focusIndex == len(m.update.inputs) {
 				// update the label
 				metadata := &common.MetadataMutable{Labels: m.attr.Metadata.Labels}
 				oldKey := m.label.title
-				// oldVal := m.label.description
 				newKey := m.update.inputs[0].Value()
 				newVal := m.update.inputs[1].Value()
 				if oldKey != newKey {
 					delete(metadata.Labels, oldKey)
 				}
 				metadata.Labels[newKey] = newVal
-				// metadata := common.MetadataMutable{Labels: map[string]string{"abc": "def"}}
 				behavior := common.MetadataUpdateEnum_METADATA_UPDATE_ENUM_REPLACE
-				// behavior := common.MetadataUpdateEnum_METADATA_UPDATE_ENUM_EXTEND
 				attr, err := m.sdk.UpdateAttribute(m.attr.Id, metadata, behavior)
 				if err != nil {
 					// return error view
