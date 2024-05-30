898a017819f89d6279b4bec3f0e0aa8eed916bca
Krish Suchak <suchak.krish@gmail.com>
2024-05-30 15:22:29 -0400

    try oktokit directly

---
+++ membership.js
@@ -0,0 +1,20 @@
+const { Octokit } = require("@octokit/rest");
+
+const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+});
+
+const membership = async (org, team_slug, username) => {
+    try {
+        const { data } = await octokit.teams.getMembershipForUserInOrg({
+            org,
+            team_slug: team_slug || 'all',
+            username,
+        });
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+membership('opentdf', 'cli', 'suchak1');
