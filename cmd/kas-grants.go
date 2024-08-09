6cd65822950260f3a901dab3ec3fe53cc930f312
jakedoublev <jake.vanvorhis@virtru.com>
2024-08-09 16:01:11 -0700

    update kas grants

--- cmd/kas-grants.go
+++ cmd/kas-grants.go
@@ -1,22 +1,24 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/opentdf/otdfctl/pkg/cli"
 	"github.com/opentdf/otdfctl/pkg/man"
 	"github.com/spf13/cobra"
 )
 
-func policy_updateKasGrant(cmd *cobra.Command, args []string) {
+func policy_assignKasGrant(cmd *cobra.Command, args []string) {
 	h := NewHandler(cmd)
 	defer h.Close()
 
 	flagHelper := cli.NewFlagHelper(cmd)
 
-	attr := flagHelper.GetOptionalString("attribute-id")
-	val := flagHelper.GetOptionalString("value-id")
-	kas := flagHelper.GetRequiredString("kas-id")
-	if attr == "" && val == "" {
-		cli.ExitWithError("Must specify and Attribute Definition id or Value id to update.", nil)
+	attrID := flagHelper.GetOptionalString("attribute-id")
+	valID := flagHelper.GetOptionalString("value-id")
+	kasID := flagHelper.GetRequiredString("kas-id")
+	if attrID == "" && valID == "" {
+		cli.ExitWithError("Must specify and Attribute Definition ID or Value ID to assign a KAS Grant.", nil)
 	}
 	var (
 		id     string
@@ -25,70 +27,91 @@ func policy_updateKasGrant(cmd *cobra.Command, args []string) {
 		err    error
 	)
 
-	if attr != "" {
-		res, err = h.UpdateKasGrantForAttribute(attr, kas)
+	if attrID != "" {
+		res, err = h.UpdateKasGrantForAttribute(attrID, kasID)
 		if err != nil {
-			cli.ExitWithError("Failed to update KAS grant for attribute", err)
+			cli.ExitWithError("Failed to assign KAS Grant for Attribute Definition", err)
 		}
-		id = attr
+		id = attrID
 		header = "Attribute ID"
 	} else {
-		res, err = h.UpdateKasGrantForValue(val, kas)
+		res, err = h.UpdateKasGrantForValue(valID, kasID)
 		if err != nil {
-			cli.ExitWithError("Failed to update KAS grant for attribute value", err)
+			cli.ExitWithError("Failed to assign KAS Grant for Attribute Value", err)
 		}
-		id = val
+		id = attrID
 		header = "Value ID"
 	}
 
-	t := cli.NewTabular([]string{header, id}, []string{"KAS ID", kas})
+	t := cli.NewTabular([]string{header, id}, []string{"KAS ID", kasID})
 	HandleSuccess(cmd, id, t, res)
 }
 
-func policy_deleteKasGrant(cmd *cobra.Command, args []string) {
+func policy_removeKasGrant(cmd *cobra.Command, args []string) {
 	h := NewHandler(cmd)
 	defer h.Close()
 
 	flagHelper := cli.NewFlagHelper(cmd)
-	attr := flagHelper.GetOptionalString("attribute-id")
-	val := flagHelper.GetOptionalString("value-id")
-	kas := flagHelper.GetRequiredString("kas-id")
+	attrID := flagHelper.GetOptionalString("attribute-id")
+	valID := flagHelper.GetOptionalString("value-id")
+	kasID := flagHelper.GetRequiredString("kas-id")
 
-	if attr == "" && val == "" {
-		cli.ExitWithError("Must specify and Attribute Definition id or Value id to delete.", nil)
+	if attrID == "" && valID == "" {
+		cli.ExitWithError("Must specify an Attribute Definition ID or Value ID to remove.", nil)
 	}
 	var (
-		id     string
-		header string
-		res    interface{}
-		err    error
+		res     interface{}
+		err     error
+		confirm string
+		rowID   []string
+		rowFQN  []string
 	)
 
-	cli.ConfirmAction(cli.ActionDelete, "KAS ID: ", kas)
+	kas, err := h.GetKasRegistryEntry(kasID)
+	if err != nil || kas == nil {
+		cli.ExitWithError("Failed to get registered KAS", err)
+	}
+	kasURI := kas.GetUri()
 
-	if attr != "" {
-		res, err = h.DeleteKasGrantFromAttribute(attr, kas)
+	if attrID != "" {
+		attr, err := h.GetAttribute(attrID)
+		if err != nil || attr == nil {
+			cli.ExitWithError("Failed to get attribute definition", err)
+		}
+		confirm = fmt.Sprintf("the grant to attribute FQN (%s) of KAS URI", attr.GetFqn())
+		cli.ConfirmAction(cli.ActionDelete, confirm, kasURI)
+		res, err = h.DeleteKasGrantFromAttribute(attrID, kasID)
 		if err != nil {
 			cli.ExitWithError("Failed to update KAS grant for attribute", err)
 		}
-		id = attr
-		header = "Attribute ID"
+
+		rowID = []string{"Attribute ID", attrID}
+		rowFQN = []string{"Attribute FQN", attr.GetFqn()}
 	} else {
-		_, err := h.DeleteKasGrantFromValue(val, kas)
+		val, err := h.GetAttributeValue(valID)
+		if err != nil || val == nil {
+			cli.ExitWithError("Failed to get attribute value", err)
+		}
+		confirm = fmt.Sprintf("the grant to attribute value FQN (%s) of KAS URI", val.GetFqn())
+		cli.ConfirmAction(cli.ActionDelete, confirm, kasURI)
+		_, err = h.DeleteKasGrantFromValue(valID, kasID)
 		if err != nil {
 			cli.ExitWithError("Failed to update KAS grant for attribute value", err)
 		}
-		id = val
-		header = "Value ID"
+		rowID = []string{"Value ID", valID}
+		rowFQN = []string{"Value FQN", val.GetFqn()}
 	}
 
-	t := cli.NewTabular([]string{header, id}, []string{"KAS ID", kas})
-	HandleSuccess(cmd, id, t, res)
+	t := cli.NewTabular(rowID, rowFQN,
+		[]string{"KAS ID", kasID},
+		[]string{"Removed Grant for KAS URI", kasURI},
+	)
+	HandleSuccess(cmd, "", t, res)
 }
 
 func init() {
 	assignCmd := man.Docs.GetCommand("policy/kas-grants/assign",
-		man.WithRun(policy_updateKasGrant),
+		man.WithRun(policy_assignKasGrant),
 	)
 	assignCmd.Flags().StringP(
 		assignCmd.GetDocFlag("attribute-id").Name,
@@ -111,7 +134,7 @@ func init() {
 	injectLabelFlags(&assignCmd.Command, true)
 
 	removeCmd := man.Docs.GetCommand("policy/kas-grants/remove",
-		man.WithRun(policy_deleteKasGrant),
+		man.WithRun(policy_removeKasGrant),
 	)
 	removeCmd.Flags().StringP(
 		removeCmd.GetDocFlag("attribute-id").Name,
