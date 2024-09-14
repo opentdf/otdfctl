fd72a92c5c316583f71624e2055bded8e8eb37b3
Krish Suchak <suchak.krish@gmail.com>
2024-09-14 01:25:11 -0400

    convert namespaces and resource mappings

--- pkg/handlers/namespaces.go
+++ pkg/handlers/namespaces.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"connectrpc.com/connect"
 	"github.com/opentdf/platform/protocol/go/common"
 	"github.com/opentdf/platform/protocol/go/policy"
 	"github.com/opentdf/platform/protocol/go/policy/namespaces"
@@ -8,45 +9,49 @@ import (
 )
 
 func (h Handler) GetNamespace(id string) (*policy.Namespace, error) {
-	resp, err := h.sdk.Namespaces.GetNamespace(h.ctx, &namespaces.GetNamespaceRequest{
-		Id: id,
-	})
+	resp, err := h.sdk.Namespaces.GetNamespace(h.ctx, &connect.Request[namespaces.GetNamespaceRequest]{
+		Msg: &namespaces.GetNamespaceRequest{
+			Id: id,
+		}})
 	if err != nil {
 		return nil, err
 	}
 
-	return resp.GetNamespace(), nil
+	return resp.Msg.GetNamespace(), nil
 }
 
 func (h Handler) ListNamespaces(state common.ActiveStateEnum) ([]*policy.Namespace, error) {
-	resp, err := h.sdk.Namespaces.ListNamespaces(h.ctx, &namespaces.ListNamespacesRequest{State: state})
+	resp, err := h.sdk.Namespaces.ListNamespaces(h.ctx, &connect.Request[namespaces.ListNamespacesRequest]{
+		Msg: &namespaces.ListNamespacesRequest{State: state}})
 	if err != nil {
 		return nil, err
 	}
 
-	return resp.GetNamespaces(), nil
+	return resp.Msg.GetNamespaces(), nil
 }
 
 // Creates and returns the created n
 func (h Handler) CreateNamespace(name string, metadata *common.MetadataMutable) (*policy.Namespace, error) {
-	resp, err := h.sdk.Namespaces.CreateNamespace(h.ctx, &namespaces.CreateNamespaceRequest{
-		Name:     name,
-		Metadata: metadata,
-	})
+	resp, err := h.sdk.Namespaces.CreateNamespace(h.ctx, &connect.Request[namespaces.CreateNamespaceRequest]{
+		Msg: &namespaces.CreateNamespaceRequest{
+			Name:     name,
+			Metadata: metadata,
+		}})
 	if err != nil {
 		return nil, err
 	}
 
-	return h.GetNamespace(resp.GetNamespace().GetId())
+	return h.GetNamespace(resp.Msg.GetNamespace().GetId())
 }
 
 // Updates and returns the updated namespace
 func (h Handler) UpdateNamespace(id string, metadata *common.MetadataMutable, behavior common.MetadataUpdateEnum) (*policy.Namespace, error) {
-	_, err := h.sdk.Namespaces.UpdateNamespace(h.ctx, &namespaces.UpdateNamespaceRequest{
-		Id:                     id,
-		Metadata:               metadata,
-		MetadataUpdateBehavior: behavior,
-	})
+	_, err := h.sdk.Namespaces.UpdateNamespace(h.ctx, &connect.Request[namespaces.UpdateNamespaceRequest]{
+		Msg: &namespaces.UpdateNamespaceRequest{
+			Id:                     id,
+			Metadata:               metadata,
+			MetadataUpdateBehavior: behavior,
+		}})
 	if err != nil {
 		return nil, err
 	}
@@ -55,9 +60,10 @@ func (h Handler) UpdateNamespace(id string, metadata *common.MetadataMutable, be
 
 // Deactivates and returns the deactivated namespace
 func (h Handler) DeactivateNamespace(id string) (*policy.Namespace, error) {
-	_, err := h.sdk.Namespaces.DeactivateNamespace(h.ctx, &namespaces.DeactivateNamespaceRequest{
-		Id: id,
-	})
+	_, err := h.sdk.Namespaces.DeactivateNamespace(h.ctx, &connect.Request[namespaces.DeactivateNamespaceRequest]{
+		Msg: &namespaces.DeactivateNamespaceRequest{
+			Id: id,
+		}})
 	if err != nil {
 		return nil, err
 	}
@@ -67,9 +73,10 @@ func (h Handler) DeactivateNamespace(id string) (*policy.Namespace, error) {
 
 // Reactivates and returns the reactivated namespace
 func (h Handler) UnsafeReactivateNamespace(id string) (*policy.Namespace, error) {
-	_, err := h.sdk.Unsafe.UnsafeReactivateNamespace(h.ctx, &unsafe.UnsafeReactivateNamespaceRequest{
-		Id: id,
-	})
+	_, err := h.sdk.Unsafe.UnsafeReactivateNamespace(h.ctx, &connect.Request[unsafe.UnsafeReactivateNamespaceRequest]{
+		Msg: &unsafe.UnsafeReactivateNamespaceRequest{
+			Id: id,
+		}})
 	if err != nil {
 		return nil, err
 	}
@@ -79,19 +86,21 @@ func (h Handler) UnsafeReactivateNamespace(id string) (*policy.Namespace, error)
 
 // Deletes and returns the deleted namespace
 func (h Handler) UnsafeDeleteNamespace(id string, fqn string) error {
-	_, err := h.sdk.Unsafe.UnsafeDeleteNamespace(h.ctx, &unsafe.UnsafeDeleteNamespaceRequest{
-		Id:  id,
-		Fqn: fqn,
-	})
+	_, err := h.sdk.Unsafe.UnsafeDeleteNamespace(h.ctx, &connect.Request[unsafe.UnsafeDeleteNamespaceRequest]{
+		Msg: &unsafe.UnsafeDeleteNamespaceRequest{
+			Id:  id,
+			Fqn: fqn,
+		}})
 	return err
 }
 
 // Unsafely updates the namespace and returns the renamed namespace
 func (h Handler) UnsafeUpdateNamespace(id, name string) (*policy.Namespace, error) {
-	_, err := h.sdk.Unsafe.UnsafeUpdateNamespace(h.ctx, &unsafe.UnsafeUpdateNamespaceRequest{
-		Id:   id,
-		Name: name,
-	})
+	_, err := h.sdk.Unsafe.UnsafeUpdateNamespace(h.ctx, &connect.Request[unsafe.UnsafeUpdateNamespaceRequest]{
+		Msg: &unsafe.UnsafeUpdateNamespaceRequest{
+			Id:   id,
+			Name: name,
+		}})
 	if err != nil {
 		return nil, err
 	}
--- pkg/handlers/resourceMappings.go
+++ pkg/handlers/resourceMappings.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 
+	"connectrpc.com/connect"
 	"github.com/opentdf/platform/protocol/go/common"
 	"github.com/opentdf/platform/protocol/go/policy"
 	"github.com/opentdf/platform/protocol/go/policy/resourcemapping"
@@ -16,48 +17,52 @@ type ResourceMapping struct {
 
 // Creates and returns the created resource mapping
 func (h *Handler) CreateResourceMapping(attributeId string, terms []string, metadata *common.MetadataMutable) (*policy.ResourceMapping, error) {
-	res, err := h.sdk.ResourceMapping.CreateResourceMapping(context.Background(), &resourcemapping.CreateResourceMappingRequest{
-		AttributeValueId: attributeId,
-		Terms:            terms,
-		Metadata:         metadata,
-	})
+	res, err := h.sdk.ResourceMapping.CreateResourceMapping(context.Background(), &connect.Request[resourcemapping.CreateResourceMappingRequest]{
+		Msg: &resourcemapping.CreateResourceMappingRequest{
+			AttributeValueId: attributeId,
+			Terms:            terms,
+			Metadata:         metadata,
+		}})
 	if err != nil {
 		return nil, err
 	}
 
-	return h.GetResourceMapping(res.GetResourceMapping().GetId())
+	return h.GetResourceMapping(res.Msg.GetResourceMapping().GetId())
 }
 
 func (h *Handler) GetResourceMapping(id string) (*policy.ResourceMapping, error) {
-	res, err := h.sdk.ResourceMapping.GetResourceMapping(context.Background(), &resourcemapping.GetResourceMappingRequest{
-		Id: id,
-	})
+	res, err := h.sdk.ResourceMapping.GetResourceMapping(context.Background(), &connect.Request[resourcemapping.GetResourceMappingRequest]{
+		Msg: &resourcemapping.GetResourceMappingRequest{
+			Id: id,
+		}})
 	if err != nil {
 		return nil, err
 	}
 
-	return res.GetResourceMapping(), nil
+	return res.Msg.GetResourceMapping(), nil
 }
 
 func (h *Handler) ListResourceMappings() ([]*policy.ResourceMapping, error) {
-	res, err := h.sdk.ResourceMapping.ListResourceMappings(context.Background(), &resourcemapping.ListResourceMappingsRequest{})
+	res, err := h.sdk.ResourceMapping.ListResourceMappings(context.Background(), &connect.Request[resourcemapping.ListResourceMappingsRequest]{
+		Msg: &resourcemapping.ListResourceMappingsRequest{}})
 	if err != nil {
 		return nil, err
 	}
 
-	return res.GetResourceMappings(), nil
+	return res.Msg.GetResourceMappings(), nil
 }
 
 // TODO: verify updation behavior
 // Updates and returns the updated resource mapping
 func (h *Handler) UpdateResourceMapping(id string, attrValueId string, terms []string, metadata *common.MetadataMutable, behavior common.MetadataUpdateEnum) (*policy.ResourceMapping, error) {
-	_, err := h.sdk.ResourceMapping.UpdateResourceMapping(context.Background(), &resourcemapping.UpdateResourceMappingRequest{
-		Id:                     id,
-		AttributeValueId:       attrValueId,
-		Terms:                  terms,
-		Metadata:               metadata,
-		MetadataUpdateBehavior: behavior,
-	})
+	_, err := h.sdk.ResourceMapping.UpdateResourceMapping(context.Background(), &connect.Request[resourcemapping.UpdateResourceMappingRequest]{
+		Msg: &resourcemapping.UpdateResourceMappingRequest{
+			Id:                     id,
+			AttributeValueId:       attrValueId,
+			Terms:                  terms,
+			Metadata:               metadata,
+			MetadataUpdateBehavior: behavior,
+		}})
 	if err != nil {
 		return nil, err
 	}
@@ -66,12 +71,13 @@ func (h *Handler) UpdateResourceMapping(id string, attrValueId string, terms []s
 }
 
 func (h *Handler) DeleteResourceMapping(id string) (*policy.ResourceMapping, error) {
-	resp, err := h.sdk.ResourceMapping.DeleteResourceMapping(context.Background(), &resourcemapping.DeleteResourceMappingRequest{
-		Id: id,
-	})
+	resp, err := h.sdk.ResourceMapping.DeleteResourceMapping(context.Background(), &connect.Request[resourcemapping.DeleteResourceMappingRequest]{
+		Msg: &resourcemapping.DeleteResourceMappingRequest{
+			Id: id,
+		}})
 	if err != nil {
 		return nil, err
 	}
 
-	return resp.GetResourceMapping(), nil
+	return resp.Msg.GetResourceMapping(), nil
 }
