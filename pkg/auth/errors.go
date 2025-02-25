7f94aae326de7390b52c23dd14dc3ede65823bb0
Jake Van Vorhis <jake.vanvorhis@virtru.com>
2025-02-25 13:02:26 -0800

    lint

--- pkg/auth/errors.go
+++ pkg/auth/errors.go
@@ -3,12 +3,11 @@ package auth
 import "errors"
 
 var (
-	ErrAccessTokenExpired        = errors.New("access token expired")
-	ErrAccessTokenNotFound       = errors.New("no access token found")
-	ErrClientCredentialsNotFound = errors.New("client credentials not found")
-	ErrInvalidAuthType           = errors.New("invalid auth type")
-	ErrUnauthenticated           = errors.New("not logged in")
-	ErrParsingAccessToken        = errors.New("failed to parse access token")
+	ErrAccessTokenExpired         = errors.New("access token expired")
+	ErrAccessTokenNotFound        = errors.New("no access token found")
+	ErrClientCredentialsNotFound  = errors.New("client credentials not found")
+	ErrInvalidAuthType            = errors.New("invalid auth type")
+	ErrUnauthenticated            = errors.New("not logged in")
+	ErrParsingAccessToken         = errors.New("failed to parse access token")
 	ErrProfileCredentialsNotFound = errors.New("profile missing credentials")
 )
-
