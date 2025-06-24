e99dfb7bba252cd87e656a22279071f3b69bd948
Ryan Schumacher <j.r.schumacher@gmail.com>
2025-06-24 17:34:19 -0500

    Allow args to support array of objects or array of strings

--- pkg/man/docflags.go
+++ pkg/man/docflags.go
@@ -14,6 +14,50 @@ type DocFlag struct {
 	Enum        []string `yaml:"enum"`
 }
 
+type DocArgument struct {
+	Name        string `yaml:"name"`
+	Description string `yaml:"description"`
+	Required    bool   `yaml:"required"`
+	Type        string `yaml:"type"`
+}
+
+// FlexibleArgs can handle both []string and []DocArgument formats
+type FlexibleArgs struct {
+	StringArgs []string
+	ObjectArgs []DocArgument
+}
+
+func (f *FlexibleArgs) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	// Try to unmarshal as []string first
+	var stringArgs []string
+	if err := unmarshal(&stringArgs); err == nil {
+		f.StringArgs = stringArgs
+		return nil
+	}
+
+	// If that fails, try to unmarshal as []DocArgument
+	var objectArgs []DocArgument
+	if err := unmarshal(&objectArgs); err == nil {
+		f.ObjectArgs = objectArgs
+		return nil
+	}
+
+	return fmt.Errorf("arguments must be either a list of strings or a list of argument objects")
+}
+
+// ToStringSlice converts FlexibleArgs to a simple string slice for compatibility
+func (f *FlexibleArgs) ToStringSlice() []string {
+	if len(f.StringArgs) > 0 {
+		return f.StringArgs
+	}
+	
+	result := make([]string, len(f.ObjectArgs))
+	for i, arg := range f.ObjectArgs {
+		result[i] = arg.Name
+	}
+	return result
+}
+
 func (d *Doc) GetDocFlag(name string) DocFlag {
 	for _, f := range d.DocFlags {
 		if f.Name == name {
